190a84d656b5be831b42425e2173783c47eac38c
amchampoux <amchampoux@mirego.com>
2022-12-13 00:12:18 -0500

    add dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,73 @@
+/* eslint-disable no-undef */
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(()=> {
+
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const renderTweets = function(tweets) {
+    let output;
+    // loops through tweets
+    for (let tweet of tweets) {
+      // calls createTweetElement for each tweet
+      output = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container
+      $('#tweets-container').append(output);
+    }
+  };
+
+  const createTweetElement = function(tweetData) {
+    let $tweet = `
+      <article class="tweet">
+        <header class="tweet__header">
+          <div class="tweet__header__user">
+            <img class="tweet__header__user__image" src=${tweetData.user.avatars}> 
+            <label class="tweet__header__user__name">${tweetData.user.name}e</label>
+          </div>
+          <label class="tweet__header__user__alias">${tweetData.user.handle}</label>
+        </header>
+        <p class="tweet__tweet">${tweetData.content.text}</p>
+        <hr>
+        <footer class="tweet__footer">
+          <label class="tweet__footer__date">${tweetData.created_at}o</label>
+          <div class="tweet__footer__social">
+            <i class="fa-brands fa-square-facebook fa-sm"></i>
+            <i class="fa-solid fa-share-from-square fa-sm"></i>
+            <i class="fa-solid fa-heart fa-sm"></i>
+          </div>
+        </footer>
+      </article>
+  `;
+    return $tweet;
+  };
+
+  renderTweets(tweetData);
+
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,54 +1,47 @@
 /* eslint-disable no-undef */
 
-const $form = $('form');
-const $input = $('#tweet-text');
-const $counter = $('output');
+$(document).ready(()=> {
 
-$form.on('input', 'input:text', function() {
+  const $form = $('form');
+  const $input = $('#tweet-text');
+  const $counter = $('output');
 
-  const initialValue = 140;
-  let counterValue = initialValue - $input.val().length;
-  $counter.html(counterValue);
-  if (counterValue < 0) {
-    $counter.addClass("tweet-form__bottom__counter--negative");
-  } else if (counterValue > 0) {
-    $counter.reset();
-  }
+  $form.on('input', 'input:text', function() {
+
+    const initialValue = 140;
+    let counterValue = initialValue - $input.val().length;
+    $counter.html(counterValue);
+    if (counterValue < 0) {
+      $counter.addClass("tweet-form__bottom__counter--negative");
+    }
+  });
+
+  // JS implementation
+
+  // const form = document.querySelector('form');
+  // const input = document.getElementById('tweet-text');
+  // const counter = document.querySelector('output');
+
+
+  // form.addEventListener('input', function() {
+
+  //   // get current counter value
+  //   let counterValue = counter.value;
+  //   counterValue = 10;
+
+  //   // get user input in textfield
+  //   const userInput = input.value;
+
+  //   // decrease the counter
+  //   counter.innerHTML = counterValue -= userInput.length;
+
+  //   // if counter value < 0, display red
+
+  //   if (counterValue > 0) {
+
+  //     counter.style.content = "/form.css";
+  //   }
+
+  // });
+
 });
-
-// JS implementation
-
-// const form = document.querySelector('form');
-// const input = document.getElementById('tweet-text');
-// const counter = document.querySelector('output');
-
-
-// form.addEventListener('input', function() {
-
-//   // get current counter value
-//   let counterValue = counter.value;
-//   counterValue = 10;
-
-//   // get user input in textfield
-//   const userInput = input.value;
-
-//   // decrease the counter
-//   counter.innerHTML = counterValue -= userInput.length;
-
-//   // if counter value < 0, display red
-
-//   if (counterValue > 0) {
-
-//     counter.style.content = "/form.css";
-//   }
-
-// });
-
-
-
-
-
-
-
-
-

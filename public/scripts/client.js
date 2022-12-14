08c2fb124ae4dd2176d3973c996c223b90442e5a
amchampoux <amchampoux@mirego.com>
2022-12-13 23:20:12 -0500

    add GET request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,35 +9,11 @@ $(document).ready(()=> {
 
   const $form = $('form');
 
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
   const renderTweets = function(tweets) {
     let output;
     // loops through tweets
     for (let tweet of tweets) {
+      tweet.created_at = timeago.format(tweet.created_at);
       // calls createTweetElement for each tweet
       output = createTweetElement(tweet);
       // takes return value and appends it to the tweets container
@@ -58,7 +34,7 @@ $(document).ready(()=> {
         <p class="tweet__tweet">${tweetData.content.text}</p>
         <hr>
         <footer class="tweet__footer">
-          <label class="tweet__footer__date">${tweetData.created_at}o</label>
+          <label class="tweet__footer__date">${tweetData.created_at}</label>
           <div class="tweet__footer__social">
             <i class="fa-brands fa-square-facebook fa-sm"></i>
             <i class="fa-solid fa-share-from-square fa-sm"></i>
@@ -69,8 +45,6 @@ $(document).ready(()=> {
   `;
     return $tweet;
   };
-
-  // renderTweets(tweetData);
 
   const loadtweets = function() {
     $.get('/tweets', (tweets) => {
